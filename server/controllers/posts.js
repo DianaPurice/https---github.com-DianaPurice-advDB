a329973023c0650d0ae1f90b17c415427d446179
Diana Purice <5purid83@solent.ac.uk>
2022-04-25 12:54:55 +0100

    logic for creating and getting products

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,3 +1,24 @@
-export const getPosts = (req, res) => {
-  res.send("this works");
+import PostProduct from "../models/postProducts.js";
+
+export const getProducts = async (req, res) => {
+  try {
+    const postProducts = await PostProduct.find();
+
+    res.status(200).json(postProducts);
+  } catch (error) {
+    res.status(404).json({ message: "srv/controllers/posts.js getProducts" });
+  }
+};
+
+export const createPost = async (req, res) => {
+  const product = req.body;
+
+  const newProduct = new PostProduct(product);
+  try {
+    await newProduct.save();
+
+    res.status(201).json(newProduct);
+  } catch (error) {
+    res.status(409).json({ message: "svr/controllers/posts.js createPost" });
+  }
 };
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { getPosts } from "../controllers/posts.js";
+import { getProducts, createPost } from "../controllers/posts.js";
 
 const router = express.Router();
 
-router.get("/", getPosts);
+router.get("/", getProducts);
+router.post("/", createPost);
 
 export default router;
